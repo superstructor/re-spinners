9d5db5d01ec59147e462c8ec3e226121015e57d8
Isaac Johnston <isaac@superstruct.nz>
2021-04-11 15:29:36 +1200

    Upgrade shadow-cljs to 2.12.4 and ClojureScript to 1.10.844

--- src/deps.cljs
+++ src/deps.cljs
@@ -1,7 +1,7 @@
 {:npm-deps     {"react-spinners"        "0.11.0-alpha.8"
                 "@emotion/core"         "11.0.0"
                 "@emotion/react"        "11.1.5"}
- :npm-dev-deps {"shadow-cljs"           "2.11.26"
+ :npm-dev-deps {"shadow-cljs"           "2.12.4"
                 "karma"                 "6.2.0"
                 "karma-chrome-launcher" "3.1.0"
                 "karma-cljs-test"       "0.1.0"
