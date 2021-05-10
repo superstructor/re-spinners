cb5f8d592370a3c85d2c6e3d44a8e441d5c2aa51
Isaac Johnston <isaac@superstruct.nz>
2021-05-10 22:22:55 +1200

    Upgrade shadow-cljs to 2.12.5

--- src/deps.cljs
+++ src/deps.cljs
@@ -1,7 +1,7 @@
 {:npm-deps     {"react-spinners"        "0.11.0-alpha.8"
                 "@emotion/core"         "11.0.0"
                 "@emotion/react"        "11.1.5"}
- :npm-dev-deps {"shadow-cljs"           "2.12.4"
+ :npm-dev-deps {"shadow-cljs"           "2.12.5"
                 "karma"                 "6.3.2"
                 "karma-chrome-launcher" "3.1.0"
                 "karma-cljs-test"       "0.1.0"
