45917859bf19c1b8f6be29f364fa9204f092d9c1
Isaac Johnston <isaac@superstruct.nz>
2021-05-10 22:27:55 +1200

    Upgrade @emotion/react to 11.4.0

--- src/deps.cljs
+++ src/deps.cljs
@@ -1,6 +1,6 @@
 {:npm-deps     {"react-spinners"        "0.11.0-beta.1"
                 "@emotion/core"         "11.0.0"
-                "@emotion/react"        "11.1.5"}
+                "@emotion/react"        "11.4.0"}
  :npm-dev-deps {"shadow-cljs"           "2.12.5"
                 "karma"                 "6.3.2"
                 "karma-chrome-launcher" "3.1.0"
