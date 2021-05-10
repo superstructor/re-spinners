cd3725338852d3848852059b18147a5d3edd9d3b
Isaac Johnston <isaac@superstruct.nz>
2021-05-10 22:25:07 +1200

    Upgrade react-spinners to 0.11.0-beta.1

--- src/deps.cljs
+++ src/deps.cljs
@@ -1,4 +1,4 @@
-{:npm-deps     {"react-spinners"        "0.11.0-alpha.8"
+{:npm-deps     {"react-spinners"        "0.11.0-beta.1"
                 "@emotion/core"         "11.0.0"
                 "@emotion/react"        "11.1.5"}
  :npm-dev-deps {"shadow-cljs"           "2.12.5"
