6bc70667b9954c010b6de877ea8e24338119e0c7
Isaac Johnston <isaac@superstruct.nz>
2021-06-24 22:29:54 +1200

    Upgrade development dependencies

--- src/deps.cljs
+++ src/deps.cljs
@@ -1,8 +1,10 @@
 {:npm-deps     {"react-spinners"        "0.11.0"
                 "@emotion/core"         "11.0.0"
                 "@emotion/react"        "11.4.0"}
- :npm-dev-deps {"shadow-cljs"           "2.12.5"
-                "karma"                 "6.3.2"
+ :npm-dev-deps {"react"                 "17.0.2"
+                "react-dom"             "17.0.2"
+                "shadow-cljs"           "2.14.5"
+                "karma"                 "6.3.4"
                 "karma-chrome-launcher" "3.1.0"
                 "karma-cljs-test"       "0.1.0"
                 "karma-junit-reporter"  "2.0.1"}}
